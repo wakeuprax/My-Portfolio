8f3be219329c619cf6d741454a8186be3fbae6db
Pushkar <pushkarverma6628@gmail.com>
2024-10-01 18:40:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function navAnimation(){
+    
+var nav=document.querySelector('nav')
+
+nav.addEventListener('mouseenter',function(){
+    let tl=gsap.timeline()
+    tl.to('#slider',{
+        height:"20vh"
+    })
+    tl.to('.one h5',{
+        display:'block'
+    })
+    tl.to('.one h5 span',{
+        y:0,
+        stagger:{
+            amount:0.5
+        }
+    })
+})
+
+
+
+nav.addEventListener('mouseleave',function(){
+    let tl=gsap.timeline()
+    tl.to('.one h5 span',{
+        y:25,   
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to('.one h5',{
+        display:"none",
+        duration:0.1
+    })
+    tl.to('#slider',{
+        height:0,
+        duration:0.2
+    })
+})
+    
+
+}
+
+navAnimation()
+
+Initialisze  tl.to
+
+function navAnimation(){
+    
+}
